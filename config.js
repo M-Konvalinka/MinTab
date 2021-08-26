9a199e3fcd04dcf11b54bf652820f5a0d618ed28
Michael Konvalinka <Mikekonvalinka@gmail.com>
2021-08-26 01:04:10 -0500

    Update config.js

--- config.js
+++ config.js
@@ -39,11 +39,7 @@ const LIST_ITEMS = [
     link: "https://www.netflix.com/"
   },
   {
-    name: "r/unixporn",
-    link: "https://www.reddit.com/r/unixporn/"
-  },
-  {
-    name: "r/firefoxcss",
-    link: "https://www.reddit.com/r/firefoxcss/"
+    name: "AWS",
+    link: "signin.aws.amazon.com"
   },
 ]
